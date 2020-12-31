808e33a2312bdab7f8e6dc785e9f0bf6154d5db5
Daniel Paul Carbone <daniel.p.carbone@gmail.com>
2020-12-31 12:08:03 -0600

    updating copyright

--- src/JSONModeler/Languages/GO/GOParser.php
+++ src/JSONModeler/Languages/GO/GOParser.php
@@ -1,7 +1,7 @@
 <?php namespace DCarbone\JSONModeler\Languages\GO;
 
 /*
- * Copyright (C) 2016-2018 Daniel Carbone (daniel.p.carbone@gmail.com)
+ * Copyright (C) 2016-2020 Daniel Carbone (daniel.p.carbone@gmail.com)
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
